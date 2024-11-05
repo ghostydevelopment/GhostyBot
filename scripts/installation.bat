60a482da9207bed7a9baac2e3a84c6e6cc65a249
mattythedev01 <hiltonroseb@gmail.com>
2024-11-05 07:18:31 -0500

    designed better

--- scripts/installation.bat
+++ scripts/installation.bat
@@ -1,10 +1,23 @@
 @echo off
-echo Installing dependencies...
+echo ================================
+echo      Installing Dependencies     
+echo ================================
 cd ..
 if exist yarn.lock (
+    echo Using Yarn to install packages...
     yarn install
 ) else (
+    echo Using NPM to install packages...
     npm install
 )
-echo Done.
-pause
+echo ================================
+echo          Installation Done!      
+echo ================================
+
+echo Counting down to exit...
+for /L %%i in (5,-1,1) do (
+    echo %%i...
+    timeout /t 1 >nul
+)
+echo 0... Exiting now.
+exit
