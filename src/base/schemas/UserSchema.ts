408e0c9a9d2391ec825e45c881dc57c4d3c7b0cb
mattythedev01 <hiltonroseb@gmail.com>
2024-11-06 18:56:54 -0500

    add tos

--- src/base/schemas/UserSchema.ts
+++ src/base/schemas/UserSchema.ts
@@ -4,12 +4,14 @@ interface IUser {
   userId: string;
   permissions: string[];
   botStaffAuthorized: boolean;
+  tos: boolean; // Added tos field
 }
 
 const UserSchema = new Schema<IUser>({
   userId: { type: String, required: true, unique: true },
   permissions: [{ type: String, required: true }],
   botStaffAuthorized: { type: Boolean, required: true },
+  tos: { type: Boolean, required: true }, // Added tos field to schema
 });
 
 const User = model<IUser>("User", UserSchema);
